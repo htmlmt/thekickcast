0097e94fa943124699e326f3eb0ec6761c499bc1
Michael Todd <mikejtodd@gmail.com>
2022-10-08 14:03:35 -0500

    #8 - Added header components

---
+++ src/components/headers/SiteHeader.jsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+import { SearchForm } from '@/components/forms/SearchForm';
+
+import { LogoImage } from '@/components/images/LogoImage';
+
+import { PageWrapper } from '@/components/wrappers/PageWrapper';
+import { SpacingWrapper } from '@/components/wrappers/SpacingWrapper';
+
+export function SiteHeader() {
+	return (
+		<header className="bg-white bg-header-pattern bg-24 py-6">
+			<PageWrapper>
+				<SpacingWrapper className="flex flex-wrap gap-x-8 justify-center md:justify-between items-end gap-y-6">
+					<Link href="/">
+						<LogoImage />
+					</Link>
+
+					<SearchForm />
+				</SpacingWrapper>
+			</PageWrapper>
+		</header>
+	)
+}
