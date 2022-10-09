f256e525ecd07d44e3129b9c2391e248962c8339
Michael Todd <mikejtodd@gmail.com>
2022-10-08 21:44:27 -0500

    #24 - Updated class to className in the ContentCard, EpisodeCard, and EpisodesCollection components

--- src/components/collections/EpisodesCollection.jsx
+++ src/components/collections/EpisodesCollection.jsx
@@ -6,18 +6,18 @@ import { SpacingWrapper } from '@/components/wrappers/SpacingWrapper';
 
 export function EpisodesCollection({episodes}) {
 	return (
-		<div class="overflow-auto c-episodes-collection">
+		<div className="overflow-auto c-episodes-collection">
 			<SpacingWrapper className="relative z-20 translate-y-1/2">
 				<EyebrowText>Latest Episodes</EyebrowText>
 			</SpacingWrapper>
 
-			<div class="flex overflow-visible items-stretch gap-x-2">
+			<div className="flex overflow-visible items-stretch gap-x-2">
 				{episodes.map((episode) => (
 					<EpisodeCard
 						episodeNumber={episode.episodeNumber.episodeNumber}
 						headingText={episode.title}
 						imageUrl={episode.featuredImage?.node.sourceUrl}
-						key={episode.id}
+						key={episode.slug}
 						link={`/episodes/${episode.slug}`}
 					/>
 				))}
