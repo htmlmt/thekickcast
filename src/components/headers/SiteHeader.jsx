5ba281ccb5d039f32d5bbf900fbb968b0e7d7393
Michael Todd <hello@michaeljamestodd.com>
2024-05-17 06:13:26 -0500

    Add social icons

--- src/components/headers/SiteHeader.jsx
+++ src/components/headers/SiteHeader.jsx
@@ -5,6 +5,13 @@ import { LogoImage } from '@/components/images/LogoImage';
 import { PageWrapper } from '@/components/wrappers/PageWrapper';
 import { SpacingWrapper } from '@/components/wrappers/SpacingWrapper';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+	faInstagram,
+	faXTwitter,
+	faTiktok,
+} from '@fortawesome/free-brands-svg-icons';
+
 export function SiteHeader() {
 	return (
 		<header className="bg-white py-6">
@@ -14,7 +21,31 @@ export function SiteHeader() {
 						<LogoImage />
 					</Link>
 
-					<nav className="flex flex flex-wrap items-center justify-end justify-center gap-x-6 gap-y-6">
+					<nav className="grid grid-cols-3 gap-6 text-center">
+						<div className="col-span-3 flex justify-end gap-6">
+							<a
+								aria-label="The Kick on Instagram"
+								href="https://www.instagram.com/thekick_pod/"
+								target="_blank"
+							>
+								<FontAwesomeIcon size="2x" icon={faInstagram} />
+							</a>
+							<a
+								aria-label="The Kick on Twitter"
+								href="https://x.com/TheKick_Pod/"
+								target="_blank"
+							>
+								<FontAwesomeIcon size="2x" icon={faXTwitter} />
+							</a>
+							<a
+								aria-label="The Kick on TikTok"
+								href="https://www.tiktok.com/@the.kick.podcast"
+								target="_blank"
+							>
+								<FontAwesomeIcon size="2x" icon={faTiktok} />
+							</a>
+						</div>
+
 						<a
 							className="hidden border-b-8 border-b-secondary-400 px-6 py-4 text-xl font-bold md:block"
 							href="#episodes"
